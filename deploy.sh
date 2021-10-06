129d6033296d3a21ffcae0c3f081c99f2dfa51bf
Antoine Gorenflot <antoine@ateliom.fr>
2021-10-06 15:16:38 +0200

    test

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-echo "ok" > logs.txt
+echo "ok" > log.txt
 echo "PORT=$PORT"
 git fetch origin && git reset --hard origin/main && git clean -f -d
 GATEWAY_PORT=$PORT docker-compose -f ../docker-compose-prod.yml up --build -d
