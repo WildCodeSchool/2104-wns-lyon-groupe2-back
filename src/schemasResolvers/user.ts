dc7e76018317a567237247a6a23f7ae6c3dcc601
Nicolas Dantas <nicolas.dantas@gmail.com>
2021-10-07 09:56:06 +0200

    fixed a bug when the context is empty

--- src/schemasResolvers/user.ts
+++ src/schemasResolvers/user.ts
@@ -93,7 +93,6 @@ export const typeDef = gql`
     workspacesAdmin: [InputWorkspacesAdmin]
     reset_password_token: String
     reset_password_expires: String
-    first_connection: Boolean!
   }
   input UpdateUser {
     id: String!
